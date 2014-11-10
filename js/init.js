b3edbed66ff8059ad8f83b7903f17b2764e06beb
blurryroots <blurryroots@posteo.de>
2014-11-10 15:54:45 +0100

    bigger pic and smaller canvas

--- js/init.js
+++ js/init.js
@@ -3,8 +3,8 @@ Reveal.initialize({
 	// The "normal" size of the presentation, aspect ratio will be preserved
 	// when the presentation is scaled to fit different resolutions. Can be
 	// specified using percentage units.
-	width: 1920,
-	height: 1080,
+	width: 1024,
+	height: 768,
 
 	// Factor of the display size that should remain empty around the content
 	margin: 0.1,
